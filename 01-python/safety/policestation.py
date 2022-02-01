0b6cc594f31139502da8914f1cd2003def221216
Desmond Pun <desmondpun@dhcp-wifi-8021x-155-41-41-121.bu.edu>
2022-02-01 13:18:54 -0500

    added function to policestation

--- 01-python/safety/policestation.py
+++ 01-python/safety/policestation.py
@@ -1,3 +1,25 @@
+from PIL import Image, ImageDraw
+import PySimpleGUI as PySimpleGUI
+from PIL import Image
+import as
+import tkinter
+import io
+
 def draw_policestation():
-    print("police station not found")
-    return
+    img = Image.open("policestation.jpeg")
+    img.thumbnail((1200, 800))
+    bio = io.BytesIO()
+    img.save(bio, format="PNG")
+
+    layout = [[[sg.Text("Firestation:", font = ("Arial", 25)),],[sg.Image(data=bio.getvalue())]], [sg.Button("Don't Show Me Anymore")]]
+
+    window = sg.Window("Image", layout, size=(1200, 800))
+
+    while True:
+        event, values = window.read()
+        if event == "Don't Show Me Anymore" or event == sg.WIN_CLOSED:
+            break
+
+    window.close()
+
+draw_policestation()
