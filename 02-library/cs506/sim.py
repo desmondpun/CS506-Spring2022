4017d0df0a5fac0ad5c3e890013816857b0e3f9e
Desmond Pun <desmondpun@dhcp-wifi-8021x-168-122-212-17.bu.edu>
2022-02-08 12:53:23 -0500

    added code for manhattan distance

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -5,7 +5,10 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
 
 def jaccard_dist(x, y):
     raise NotImplementedError()
