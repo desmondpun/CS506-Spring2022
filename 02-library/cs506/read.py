9899ac6a87474fb48c0f17183055dc1cb0389886
Desmond Pun <desmondpun@dhcp-wifi-8021x-155-41-101-253.bu.edu>
2022-02-08 17:21:43 -0500

    modify csv read method

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -4,4 +4,7 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    return open(csv_file_path)
+    with open(csv_file_path, newline='') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    return data
