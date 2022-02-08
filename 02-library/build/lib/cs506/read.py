bfcd38ad13f6f1294d0175dca0f5bba11b6024db
Desmond Pun <desmondpun@dhcp-wifi-8021x-168-122-212-17.bu.edu>
2022-02-08 13:24:33 -0500

    updated version in tox

---
+++ 02-library/build/lib/cs506/read.py
@@ -0,0 +1,8 @@
+import panda as pd
+def read_csv(csv_file_path):
+    """
+        Given a path to a csv file, return a matrix (list of lists)
+        in row major.
+    """
+    df = pd.read_csv(csv_file_path)
+    return df.transpose()
---
+++ 02-library/build/lib/cs506/sim.py
@@ -0,0 +1,22 @@
+from scipy import spatial
+def euclidean_dist(x, y):
+    res = 0
+    for i in range(len(x)):
+        res += (x[i] - y[i])**2
+    return res**(1/2)
+
+def manhattan_dist(x, y):
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
+
+def jaccard_dist(x, y):
+    nom = A.intersection(B)
+    den = A.union(B)
+    dist = 1- (len(nom)/len(den))
+    return dist
+
+def cosine_sim(x, y):
+    return 1-spatial.distance.cosine(x, y)
+# Feel free to add more
