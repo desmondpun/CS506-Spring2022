9169f634d5b361d4f079418e6bcd3bfecc8c41a6
Desmond Pun <desmondpun@dhcp-wifi-8021x-168-122-200-227.bu.edu>
2022-02-14 16:36:58 -0500

    dbscan up to date as of last class

--- 02-library/cs506/dbscan.py
+++ 02-library/cs506/dbscan.py
@@ -6,11 +6,34 @@ class DBC():
         self.epsilon = epsilon
 
 
+    def epsilon_neighborhood(P):
+        #Add code here
+        return []
+
+    def explore_and_assign_eps_neighborhood(P, cluster, assignments):
+        #Add code here
+        return assignments
+
     def dbscan(self):
         """
             returns a list of assignments. The index of the
             assignment should match the index of the data point
             in the dataset.
         """
-        
-        raise NotImplementedError
+        #Add code to function
+        assignments = [0 for _ in range(len(self.dataset))]
+
+        cluster = 1
+
+        for P in range(len(self.dataset)):
+            if assignments[P] != 0:
+                #already part of a cluster
+                continue
+            if len(self.epsilon_neighborhood(P)) >= self.min_pts:
+                #core point
+                self.explore_and_assign_eps_neighborhodd(P, cluster, assignments)
+            else:
+                #border or noise
+            
+            cluster += 1
+        return assignments
